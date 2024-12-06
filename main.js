cf9e397d71dbd062cdbd06b120f91571ebc7b686
Kayes Mahmud <98325700+kayesmahmud30@users.noreply.github.com>
2024-12-06 17:12:01 +0600

    update main.js

--- main.js
+++ main.js
@@ -58,5 +58,5 @@ sr.reveal('.skills__data, .work__img, .contact__input',{interval: 200});
 var subForm = document.getElementsByClassName('contact__button button')
 var txtArea = document.getElementsByClassName('contact__input')
 subForm.onclick(){
-  
+  txtArea.
 }
